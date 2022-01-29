30f71b33bf1dba0aff7cdc650469b34c4568d6d6
devRayat <dev.rayat000@gmail.com>
2022-01-29 18:54:31 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/**
+ * @type {import('tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: ["src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
